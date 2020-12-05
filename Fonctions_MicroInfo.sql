27802b158ef77ff3ebcc41935d5e1f5aa067f39b
Jrobill4rd <robillard.jeffrey@courrier.uqam.ca>
2020-12-05 16:50:18 -0500

    Modification de fonctions

--- Fonctions_MicroInfo.sql
+++ Fonctions_MicroInfo.sql
@@ -5,30 +5,30 @@ SET ECHO ON
 -- Creation des fonctions
 SET ECHO ON
 
-CREATE FUNCTION fQuantiteDejaLivree
+CREATE OR REPLACE FUNCTION fQuantiteDejaLivree
 (unNoProduit LigneLivraison.noProduit%TYPE, unNoCommande LigneLivraison.noCommande%TYPE)
 RETURN  Commande.quantiteDejaLivree%TYPE IS 
 
-    uneQuantiteDejalivree LigneLivraison.quantiteDejaLivree%TYPE
+    uneQuantiteDejalivree LigneLivraison.quantiteDejaLivree%TYPE;
 BEGIN 
    SELECT   SUM(quantiteLivre)
    INTO     uneQuantitelivree
    FROM     LigneLivraison
-   WHERE    (noProduit = unNoProduit) AND (noCommande = unNoCommande)
+   WHERE    (noProduit = unNoProduit) AND (noCommande = unNoCommande);
    RETURN   uneQuantiteDejaLivree;
 END fQuantiteDejaLivree;
 
 /
-CREATE FUNCTION fTotalFacture
+CREATE OR REPLACE FUNCTION fTotalFacture
 (unNoLivraison Facture.noLivraison%TYPE)
-RETURN Facture.montantTotalFacture%TYPE
+RETURN Facture.montantTotalFacture%TYPE IS
 
-    unMontantTotalFacture Facture.montantTotalFacture%TYPE
+    unMontantTotalFacture Facture.montantTotalFacture%TYPE;
 BEGIN
     SELECT  SUM(montantSousTotal + montantTaxes)
     INTO    unMontantTotalFacture
     FROM    Facture
     WHERE   noLivraison = unNoLivraison;
     RETURN  unMontantTotalFacture;
-END fTotalFacture
+END fTotalFacture;
 /
