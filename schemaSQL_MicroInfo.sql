ca8400ac195a1d33128dab0b7e282339aa079079
angelie1606 <angelie1606@gmail.com>
2020-12-06 21:59:23 -0500

    Update schemaSQL_MicroInfo.sql

--- schemaSQL_MicroInfo.sql
+++ schemaSQL_MicroInfo.sql
@@ -5,89 +5,90 @@ SPOOL "Schema_Output.txt"
 
 -- Creation des tables
 CREATE TABLE Usager
-(noUsager		NUMBER(19)		NOT NULL,
- motDePasse		VARCHAR(25)		NOT NULL,
- typeUsager 	VARCHAR(15) 
+(noUsager               NUMBER(19)              NOT NULL,
+ motDePasse             VARCHAR(25)             NOT NULL,
+ typeUsager     VARCHAR(15)
  CHECK(typeUsager IN('Client', 'Fournisseur', 'Commis')),
- PRIMARY KEY	(noUsager)	
-) 
+ PRIMARY KEY    (noUsager)
+)
 /
 CREATE TABLE Fournisseur
-(noUsager		NUMBER(19)		NOT NULL,
- noFournisseur	NUMBER(19)		NOT NULL	UNIQUE,
- telephone		CHAR(10)		NOT NULL,
- raisonSociale	VARCHAR(50)		NOT NULL,
- noCivique		NUMBER(19)		NOT NULL,
- rue			VARCHAR(50)		NOT NULL,
- ville			VARCHAR(50)		NOT NULL,
- pays			VARCHAR(50)		NOT NULL,
- codePostal		VARCHAR(6)		NOT NULL,
- FOREIGN KEY	(noUsager) REFERENCES Usager
+(noUsager               NUMBER(19)              NOT NULL,
+ noFournisseur  NUMBER(19)              NOT NULL,
+ telephone              CHAR(10)                NOT NULL,
+ raisonSociale  VARCHAR(50)             NOT NULL,
+ noCivique              NUMBER(19)              NOT NULL,
+ rue                    VARCHAR(50)             NOT NULL,
+ ville                  VARCHAR(50)             NOT NULL,
+ pays                   VARCHAR(50)             NOT NULL,
+ codePostal             VARCHAR(6)              NOT NULL,
+ PRIMARY KEY    (noFournisseur),
+ FOREIGN KEY    (noUsager) REFERENCES Usager
 )
 /
 CREATE TABLE Produit
-(produitId		NUMBER(19)		NOT NULL,
- noProduit		NUMBER(19)		NOT NULL,
- codeZebre		CHAR(12)		NOT NULL,
- PRIMARY KEY	(produitId)
+(noProduit              NUMBER(19)              NOT NULL,
+ codeZebre              CHAR(12)                NOT NULL,
+ PRIMARY KEY    (noProduit)
 )
-/ 
+/
 CREATE TABLE PrioriteFournisseur
-(noFournisseur	NUMBER(19)		NOT NULL,
- noProduit		NUMBER(19)		NOT NULL,
- priorite 		NUMBER(19)		NOT NULL,
- PRIMARY KEY	(noFournisseur),
- FOREIGN KEY	(noFournisseur) REFERENCES Fournisseur,
- FOREIGN KEY    (noProduit)		REFERENCES Produit
+(noFournisseur  NUMBER(19)              NOT NULL,
+ noProduit              NUMBER(19)              NOT NULL,
+ priorite               NUMBER(19)              NOT NULL,
+ PRIMARY KEY    (noFournisseur),
+ FOREIGN KEY    (noFournisseur) REFERENCES Fournisseur,
+ FOREIGN KEY    (noProduit)             REFERENCES Produit
 )
 /
 CREATE TABLE Client
-(noClient		NUMBER(19)		NOT NULL,
- noUsager		NUMBER(19)		NOT NULL,
- prenom		    VARCHAR(50)		NOT NULL,
- nom		    VARCHAR(50)		NOT NULL,
- telephone		CHAR(10)		NOT NULL,
- qualite	    VARCHAR(25)		NOT NULL,
- noCivique		NUMBER(19)		NOT NULL,
- rue		    VARCHAR(50)		NOT NULL,
- ville		    VARCHAR(50)		NOT NULL,
- pays		    VARCHAR(50)		NOT NULL,
- codePostal		CHAR(6)			NOT NULL,
+(noClient               NUMBER(19)              NOT NULL,
+ noUsager               NUMBER(19)              NOT NULL,
+ prenom             VARCHAR(50)         NOT NULL,
+ nom                VARCHAR(50)         NOT NULL,
+ telephone              CHAR(10)                NOT NULL,
+ qualite            VARCHAR(25)         NOT NULL,
+ noCivique              NUMBER(19)              NOT NULL,
+ rue                VARCHAR(50)         NOT NULL,
+ ville              VARCHAR(50)         NOT NULL,
+ pays               VARCHAR(50)         NOT NULL,
+ codePostal             CHAR(6)                 NOT NULL,
  PRIMARY KEY (noClient),
  FOREIGN KEY (noUsager) REFERENCES Usager
 )
-/ 
-CREATE TABLE TypeProduit
-(noProduit			NUMBER(19)		NOT NULL	UNIQUE,
- description		VARCHAR(250)	NOT NULL,
- minimumEnStock		NUMBER(19)      NOT NULL,
- quantiteEnStock	NUMBER(19)      NOT NULL,
- FOREIGN KEY (noProduit) REFERENCES Produit 
-) 
 /
- 
+CREATE TABLE TypeProduit
+(noProduit                      NUMBER(19)              NOT NULL        ,
+ description            VARCHAR(250)    NOT NULL,
+ minimumEnStock         NUMBER(19)      NOT NULL,
+ quantiteEnStock        NUMBER(19)      NOT NULL,
+ PRIMARY KEY (noProduit),
+ FOREIGN KEY (noProduit) REFERENCES Produit
+)
+/
+
 CREATE TABLE ProduitPrix
-(noProduit			NUMBER(19)		NOT NULL,
- dateEnVigueur		DATE 		    NOT NULL,
- prix 				NUMBER(19,4)    NOT NULL,
+(noProduit                      NUMBER(19)              NOT NULL,
+ dateEnVigueur          DATE                NOT NULL,
+ prix                           NUMBER(19,4)    NOT NULL,
  PRIMARY KEY (noProduit),
  FOREIGN KEY (noProduit) REFERENCES Produit
 )
 /
 CREATE TABLE Commande
-(noCommande		NUMBER(19)		NOT NULL,
- noClient		NUMBER(19)		NOT NULL,
- dateCommande	DATE	     	NOT NULL,
- typeStatusCommande VARCHAR(15) 
+(noCommande             NUMBER(19)              NOT NULL,
+ noClient               NUMBER(19)              NOT NULL,
+ dateCommande   DATE            NOT NULL,
+ typeStatusCommande VARCHAR(15)
  CHECK(typeStatusCommande IN ('Annulee','Livree','Payee','En Attente')),
  PRIMARY KEY (noCommande),
  FOREIGN KEY (noClient) REFERENCES Client
 )
-/ 
+/
 CREATE TABLE LigneCommande
-(noCommande		NUMBER(19)		NOT NULL,
- noProduit		NUMBER(19)		NOT NULL,
- quantite 		NUMBER(19)		NOT NULL,
+(noCommande             NUMBER(19)              NOT NULL,
+ noProduit              NUMBER(19)              NOT NULL,
+ quantite               NUMBER(19)              NOT NULL,
  CHECK (quantite > 0),
  PRIMARY KEY (noCommande, noProduit),
  FOREIGN KEY (noCommande) REFERENCES Commande,
@@ -95,57 +96,57 @@ CREATE TABLE LigneCommande
 )
 /
 CREATE TABLE Livraison
-(noLivraison 		NUMBER(19)		NOT NULL,
- noClient			NUMBER(19)		NOT NULL,
- dateLivraison		DATE 			NOT NULL,
+(noLivraison            NUMBER(19)              NOT NULL,
+ noClient                       NUMBER(19)              NOT NULL,
+ dateLivraison          DATE                    NOT NULL,
  PRIMARY KEY (noLivraison),
  FOREIGN KEY (noClient) REFERENCES Client
 )
 /
 CREATE TABLE LigneLivraison
-(noLivraison 		NUMBER(19)		NOT NULL,
- noProduit			NUMBER(19)		NOT NULL,
- noCommande			NUMBER(19)		NOT NULL,
+(noLivraison            NUMBER(19)              NOT NULL,
+ noProduit                      NUMBER(19)              NOT NULL,
+ noCommande                     NUMBER(19)              NOT NULL,
  quantiteLivree     NUMBER(19)      NOT NULL,
  PRIMARY KEY (noLivraison),
  FOREIGN KEY (noLivraison) REFERENCES Livraison,
- FOREIGN KEY (noProduit)   REFERENCES Produit,
+ FOREIGN KEY (noProduit)   REFERENCES TypeProduit,
  FOREIGN KEY (noCommande)  REFERENCES Commande
 )
 /
 
 CREATE TABLE Facture
-(noLivraison 		NUMBER(19)		NOT NULL,
- montantSousTotal	NUMBER(19,4)	NOT NULL,
- montantTaxes 		NUMBER(19,4)	NOT NULL,
+(noLivraison            NUMBER(19)              NOT NULL,
+ montantSousTotal       NUMBER(19,4)    NOT NULL,
+ montantTaxes           NUMBER(19,4)    NOT NULL,
  dateLimitePaiment  DATE            NOT NULL,
  PRIMARY KEY (noLivraison),
  FOREIGN KEY (noLivraison) REFERENCES Livraison
  )
 /
 CREATE TABLE Paiement
-(noLivraison 		NUMBER(19)		NOT NULL,
- noPaiement 		NUMBER(19)		NOT NULL,
- datePaiement		DATE 			NOT NULL,
- montant 			FLOAT(4)        NOT NULL,
+(noLivraison            NUMBER(19)              NOT NULL,
+ noPaiement             NUMBER(19)              NOT NULL,
+ datePaiement           DATE                    NOT NULL,
+ montant                        FLOAT(4)        NOT NULL,
  PRIMARY KEY (noPaiement),
  FOREIGN KEY (noLivraison) REFERENCES Livraison
 )
 /
 CREATE TABLE PaiementCheque
-( noPaiement 		NUMBER(19)		NOT NULL,
-  noBanque 		    NUMBER(19)		NOT NULL,
-  noCompte 		    NUMBER(19)		NOT NULL,
+( noPaiement            NUMBER(19)              NOT NULL,
+  noBanque                  NUMBER(19)          NOT NULL,
+  noCompte                  NUMBER(19)          NOT NULL,
   PRIMARY KEY (noPaiement),
   FOREIGN KEY (noPaiement) REFERENCES Paiement(noPaiement)
 )
 /
 CREATE TABLE PaiementCarteCredit
-(noPaiement 		NUMBER(19)      NOT NULL,
- noCarte			VARCHAR(25)     NOT NULL,
- typeCarteCredit    VARCHAR(15) 
+(noPaiement             NUMBER(19)      NOT NULL,
+ noCarte                        VARCHAR(25)     NOT NULL,
+ typeCarteCredit    VARCHAR(15)
  CHECK (typeCarteCredit IN('Visa','MasterCard','AmericanExpress')),
- dateExpiration	    DATE  			NOT NULL,
+ dateExpiration     DATE                        NOT NULL,
  PRIMARY KEY (noPaiement),
  FOREIGN KEY (noPaiement) REFERENCES Paiement(noPaiement)
  )
@@ -180,8 +181,8 @@ BEGIN
         FROM TypeProduit
         WHERE TypeProduit.noProduit = :LivraisonStock.noProduit;
 
-        IF :LivraisonStock.quantiteLivree > quantiteStock THEN 
-		raise_application_error(-20100, 'La quantite livree ne peut depasser la quantite en stock');
+        IF :LivraisonStock.quantiteLivree > quantiteStock THEN
+                RAISE_APPLICATION_ERROR (-20100, 'La quantite livree ne peut depasser la quantite en stock');
         END IF;
 END;
 /
@@ -190,24 +191,24 @@ CREATE OR REPLACE TRIGGER bloquerInsertionCommande
 BEFORE INSERT
         ON LigneLivraison
 REFERENCING
-	NEW AS NouvelleLivraison
+        NEW AS NouvelleLivraison
 FOR EACH ROW
-	DECLARE
-		qteLivree	NUMBER(19);
-		qteCommandee	NUMBER(19);
-BEGIN	
-	SELECT	SUM(quantiteLivree)
-	INTO	qteLivree
-	FROM	LigneLivraison
-	WHERE	LigneLivraison.noProduit = :NouvelleLivraison.noProduit;
+        DECLARE
+                qteLivree       NUMBER(19);
+                qteCommandee    NUMBER(19);
+BEGIN
+        SELECT  SUM(quantiteLivree)
+        INTO    qteLivree
+        FROM    LigneLivraison
+        WHERE   LigneLivraison.noProduit = :NouvelleLivraison.noProduit;
 
-	SELECT	quantite
-	INTO	qteCommandee
-	FROM	LigneCommande
-	WHERE	LigneCommande.noProduit = :NouvelleLivraison.noProduit;
+        SELECT  quantite
+        INTO    qteCommandee
+        FROM    LigneCommande
+        WHERE   LigneCommande.noProduit = :NouvelleLivraison.noProduit;
 
-        IF :NouvelleLivraison.quantiteLivree + qteLivree > qteCommandee THEN 
-		raise_application_error(-20100, 'La quantite a livrer est trop elevee');
+        IF :NouvelleLivraison.quantiteLivree + qteLivree > qteCommandee THEN
+                raise_application_error(-20100, 'La quantite a livrer est trop elevee');
         END IF;
 END;
 /
@@ -218,22 +219,23 @@ BEFORE INSERT
 REFERENCING
         NEW AS NouveauPaiement
 FOR EACH ROW
-	DECLARE
-		totalPaiement NUMBER(19,4);
-		totalFacture  NUMBER(19,4); 
+        DECLARE
+                totalPaiement NUMBER(19,4);
+                totalFacture  NUMBER(19,4);
 BEGIN
-	SELECT  SUM(Facture.montantSousTotal + Facture.montantTaxes)
-	INTO 	totalFacture
-	FROM	Facture
-	WHERE	Facture.noLivraison = :NouveauPaiement.noLivraison;
-	
-	SELECT	SUM(montant)
-	INTO	totalPaiement
-	FROM	Paiement
-	WHERE	Paiement.noLivraison = :NouveauPaiement.noLivraison;
-	        
-        IF :NouveauPaiement.montant > (totalFacture - totalPaiement) THEN 
-        	raise_application_error(-20300, 'Le montant a payer ne doit pas depasser le montant du');
+        SELECT  SUM(Facture.montantSousTotal + Facture.montantTaxes)
+        INTO    totalFacture
+        FROM    Facture
+        WHERE   Facture.noLivraison = :NouveauPaiement.noLivraison;
+
+        SELECT  SUM(montant)
+        INTO    totalPaiement
+        FROM    Paiement
+        WHERE   Paiement.noLivraison = :NouveauPaiement.noLivraison;
+
+        IF :NouveauPaiement.montant > (totalFacture - totalPaiement) THEN
+                raise_application_error(-20300, 'Le montant a payer ne doit pas depasser le montant du');
         END IF;
 END;
 /
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   1,11          All
