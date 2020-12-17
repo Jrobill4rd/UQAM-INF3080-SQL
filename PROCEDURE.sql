4e8d7868373702174f88ff5ecb57b7c5748a82fd
angelie1606 <angelie1606@gmail.com>
2020-12-17 18:19:32 -0500

    Update PROCEDURE.sql

--- PROCEDURE.sql
+++ PROCEDURE.sql
@@ -18,68 +18,43 @@ CREATE OR REPLACE PROCEDURE DetailFacture ( no_livraison NUMBER ) IS
 
 DECLARE
 
-l_facture_no_livraison Facture.noLivraison%TYPE;
-l_facture_date_livraison Livraison.dateLivraison%TYPE;
-l_facture_no_commande Commande.noCommande%TYPE;
-l_facture_no_client Commande.noClient%TYPE;
-l_facture_no_produit LigneCommande.noProduit%TYPE;
-l_facture_quantite LigneCommande.quantite%TYPE;
-l_facture_prix_produit ProduitPrix.prix%TYPE
-l_facture_description_produit TypeProduit.description%TYPE;
-l_facture_prenom_client Client.prenom%TYPE;
-l_facture_nom_client Client.nom%TYPE;
-l_facture_telephone_client Client.telephone%TYPE;
-l_facture_noCivique_client Client.noCivique%TYPE;
-l_facture_rue_client Client.rue%TYPE;
-l_facture_ville_client Client.ville%TYPE;
-l_facture_pays_client Client.pays%TYPE;
-l_facture_codepostal_client Client.codePostal%TYPE;
+l_facture_livraison LigneCommande%ROWTYPE;
 
 CURSOR produitscommande IS
          SELECT        
-                  LigneCommande.noProduit,
-                  LigneCommande.quantite,
-                  ProduitPrix.prix,
-                  TypeProduit.description,
+                  LigneCommande.noProduit produit,
+                  LigneCommande.quantite quantite,
+                  ProduitPrix.prix prix,
+                  TypeProduit.description description
          FROM LigneCommande, ProduitPrix, TypeProduit
-         WHERE 
+         WHERE no_livraison = LigneLivraison.noLivraison AND LigneLivraison.noCommande = LigneCommande.noCommande
+         AND LigneCommande.noProduit = ProduitPrix.noProduit AND LigneCommande.noProduit = TypeProduit.noProduit ;
+                  
+ 
+ l_facture_curseur produitscommande%ROWTYPE;
+ l_facture_prixtotal ProduitPrix.prix%TYPE;
  
  BEGIN
+      OPEN produitscommande;
+      FETCH produitscommande INTO l_facture_curseur;
+      CLOSE produitscommande
+     
          SELECT
-                Facture.noLivraison,
-                Livraison.dateLivraison,
-                Commande.noCommande,
-                Commande.noClient,
-                LigneCommande.noProduit,
-                LigneCommande.quantite,
-                ProduitPrix.prix,
-                TypeProduit.description,
-                Client.prenom,
-                Client.nom,
-                Client.telephone,
-                Client.noCivique,
-                Client.rue,
-                Client.ville,
-                Client.pays,
-                Client.codePostal
+                Facture.noLivraison nolivraison,
+                Livraison.dateLivraison datelivraison,
+                Commande.noCommande nocommande,
+                Commande.noClient noclient,
+                Client.prenom prenom,
+                Client.nom nom,
+                Client.telephone telephone,
+                Client.noCivique nocivique,
+                Client.rue rue,
+                Client.ville ville,
+                Client.pays pays,
+                Client.codePostal codepostal
         INTO
-                l_facture_no_livraison,
-                l_facture_date_livraison,
-                l_facture_no_commande,
-                l_facture_no_client,
-                l_facture_no_produit,
-                l_facture_quantite,
-                l_facture_prix_produit,
-                l_facture_description_produit,
-                l_facture_prenom_client,
-                l_facture_nom_client,
-                l_facture_telephone_client,
-                l_facture_noCivique_client,
-                l_facture_rue_client,
-                l_facture_ville_client,
-                l_facture_pays_client,
-                l_facture_codepostal_client;
-
+                l_facture_livraison
+               
         FROM Facture
                 INNER JOIN Livraison
                         ON Facture.noLivraison = Livraison.noLivraison
@@ -97,28 +72,20 @@ CURSOR produitscommande IS
                         ON Commande.noClient = Client.noClient
 
         WHERE
-                noLivraison_facture = Facture.noLivraison;
+                noLivraison_facture = nolivraison;
 
-        DBMS_OUTPUT.PUT_LINE('No Client: ' || l_facture_no_client);
-        DBMS_OUTPUT.PUT_LINE('No Facture: ' || Facture.noLivraison);
-        DBMS_OUTPUT.PUT_LINE('No Livraison: ' || l_facture_no_livraison);
-        DBMS_OUTPUT.PUT_LINE('Date de la livraison: ' || l_facture_date_livraison);
+        DBMS_OUTPUT.PUT_LINE('No Client: ' || l_facture_livraison.noclient);
+        DBMS_OUTPUT.PUT_LINE('No Facture: ' || l_facture_livraison.noLivraison);
+        DBMS_OUTPUT.PUT_LINE('No Livraison: ' || l_facture_livraison.livraison);
+        DBMS_OUTPUT.PUT_LINE('Date de la livraison: ' || l_facture_livraison.datelivraison);
         DBMS_OUTPUT.PUT_LINE('Livrée à cette addresse: ');
-        DBMS_OUTPUT.PUT_LINE(l_facture_nom_client);
-        DBMS_OUTPUT.PUT_LINE(l_facture_prenom_client);
-        DBMS_OUTPUT.PUT_LINE(l_facture_noCivique_client);
-        DBMS_OUTPUT.PUT_LINE(l_facture_rue_client);
-        DBMS_OUTPUT.PUT_LINE(l_facture_ville_client);
-        DBMS_OUTPUT.PUT_LINE(l_facture_pays_client);
-        DBMS_OUTPUT.PUT_LINE(l_facture_codepostal_client);
-        DBMS_OUTPUT.PUT_LINE('Voici la liste détaillée de la commande ' || l_facture_no_commande);
-        DBMS_OUTPUT.PUT_LINE(l_facture_quantite || 'No produit: ' || l_facture_no_produit || ', prix du produit : ' || l_facture_prix_produit);
-        DBMS_OUTPUT.PUT_LINE('Description du produit: ' || l_facture_description_produit);
-
-        COMMIT;
-        CLOSE detailscommande;
+        DBMS_OUTPUT.PUT_LINE(l_facture_livraison.nom);
+        DBMS_OUTPUT.PUT_LINE(l_facture_livraison.prenom);
+        DBMS_OUTPUT.PUT_LINE(l_facture_livraison.nocivique);
+        DBMS_OUTPUT.PUT_LINE(l_facture_livraison.rue);
+        DBMS_OUTPUT.PUT_LINE(l_facture_livraison.ville);
+        DBMS_OUTPUT.PUT_LINE(l_facture_livraison.pays);
+        DBMS_OUTPUT.PUT_LINE(l_facture_livraison.codepostal);
+        DBMS_OUTPUT.PUT_LINE('Voici la liste détaillée de la commande ' || l_facture_curseur);
 
 END;
-
-
--- penser que je peux utiliser le curseur avec une loop
